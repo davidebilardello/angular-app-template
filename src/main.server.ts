2cbb521c19ad2d799d7a74036a6b97353892afac
Davide Bilardello <bilardellodavides@gmail.com>
2025-11-24 21:47:15 +0100

    Update to Angular v21

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,10 @@
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import {bootstrapApplication, BootstrapContext} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {config} from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, {
+  ...config,
+  providers: [...config.providers]
+}, context);
 
 export default bootstrap;
